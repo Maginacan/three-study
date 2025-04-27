dd34ccc7582fccf880cdf697c51c03b4a67758d7
Maginacan <48593245+Maginacan@users.noreply.github.com>
2025-04-27 19:57:34 +0800

    feat: 简单的房子绘制

--- examples/house/houseMesh.js
+++ examples/house/houseMesh.js
@@ -1,6 +1,10 @@
 import * as THREE from 'three';
 import foundationMesh from './foundation.js'
 import sideWall from './sideWall.js'
+import behindWallMesh from './behindWall.js'
+import frontWallMesh from './frontWall.js'
+import roof from './roof.js'
+import doorstep from './doorStep.js'
 
 const houseMesh = new THREE.Group();
 houseMesh.add(foundationMesh);
@@ -14,5 +18,21 @@ houseMesh.add(sideWall)
 const sideWall2 = sideWall.clone();
 sideWall2.translateZ(-4000);
 houseMesh.add(sideWall2)
+
+houseMesh.add(behindWallMesh);
+
+frontWallMesh.translateX(-2000);
+frontWallMesh.translateZ(1400);
+frontWallMesh.translateY(-150);
+houseMesh.add(frontWallMesh);
+
+houseMesh.add(roof);
+
+const roof2 = roof.clone();
+roof2.rotateX( 70 / 180 * Math.PI);
+roof2.position.z = -roof.position.z;
+houseMesh.add(roof2);
+
+houseMesh.add(doorstep);
 
 export default houseMesh;
